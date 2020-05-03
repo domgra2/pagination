4d6c8045bab9357c0fb59f04526445ea974c553f
domgra2 <domgra2@st.amu.edu.pl>
2020-05-03 16:16:20 +0200

    Update pagination.js

--- pagination.js
+++ pagination.js
@@ -6,9 +6,30 @@ const settings = {
 };
 
 const paginateArray = (dataEntries, settings) => {
+if(typeof(settings.actualPageIdx)=="number"&&typeof(settings.entriesOnPage)=="number"){
+if(settings.actualPageIdx>=1 && settings.entriesOnPage>=1 &&Number.isInteger(settings.entriesOnPage)&&Number.isInteger(settings.actualPageIdx)){
+if(Array.isArray(dataEntries)){
+if(settings.actualPageIdx*settings.entriesOnPage<=dataEntries.length){
 entriesOnSelectedPage = dataEntries.slice(((settings.actualPageIdx-1)*settings.entriesOnPage),(settings.actualPageIdx)*(settings.entriesOnPage));
-
 return entriesOnSelectedPage;
-
 }
-console.log(paginateArray(samplearray,settings));
+else{
+console.log("nie ma takiej strony");
+return 0;
+}
+}
+else{
+console.log("Zły format danych. "+typeof(dataEntries));
+return 0;
+}
+}
+else{
+console.log("Liczba elementów na stronie oraz aktualna strona musi być liczbą całkowitą dodatnią."); 
+return 0;
+}
+}
+else{
+console.log("Liczba elementów na stronie oraz aktualna strona musi być liczbą"); 
+return 0;
+}
+}
